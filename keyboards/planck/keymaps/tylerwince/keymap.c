59d4baf0ce69e7a5f2877fff84d46810dbe24c66
Tyler Wince <tyler@myndshft.com>
2019-07-12 07:21:44 -0700

    updated keymap

--- keyboards/planck/keymaps/tylerwince/keymap.c
+++ keyboards/planck/keymaps/tylerwince/keymap.c
@@ -34,15 +34,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              * |------+------+------+------+------+-------------+------+------+------+------+------|
              * |CtlEsc|A/Num |O/Sym |E/Mse |U/Arw |   I  |   D  |   H  |   T  |   N  |   S  | Bksp |
              * |------+------+------+------+------+------|------+------+------+------+------+------|
-             * |Func  |   ;  |   Q  |   J  |   K  |   X  |   B  |   M  |   W  |   V  |   Z  |Enter |
+             * | Func |   ;  |   Q  |   J  |   K  |   X  |   B  |   M  |   W  |   V  |   Z  |Enter |
              * |------+------+------+------+------+------+------+------+------+------+------+------|
              * |      | Ctrl | Alt  | GUI  |Shift |     Space   | Ctrl | Left | Down |  Up  |Right |
              * `-----------------------------------------------------------------------------------'
              */
-          KC_TAB,        KC_QUOTE,          KC_COMMA, KC_DOT,  KC_P,    KC_Y,     KC_F,  KC_G,    KC_C,    KC_R,    KC_L,    KC_SLASH,
-          CTLESC,        NumA,              SymO,     MseE,    ArwU,    KC_I,     KC_D,  KC_H,    KC_T,    KC_N,    KC_S,    KC_BSPACE,
-          MO(_FUNCTION), TD(TD_SEMI_COLON), KC_Q,     KC_J,    KC_K,    KC_X,     KC_B,  KC_M,    KC_W,    KC_V,    KC_Z,    KC_ENTER,
-          _______,       KC_LCTRL,          KC_LALT,  KC_LGUI, KC_LSFT, KC_SPACE, KC_NO, KC_RCTL, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT
+          KC_TAB,         KC_QUOTE,          KC_COMMA, KC_DOT,  KC_P,    KC_Y,     KC_F,  KC_G,    KC_C,    KC_R,    KC_L,  KC_SLASH,
+          CTLESC,         NumA,              SymO,     MseE,    ArwU,    KC_I,     KC_D,  KC_H,    KC_T,    KC_N,    KC_S,  KC_BSPACE,
+          OSL(_FUNCTION), TD(TD_SEMI_COLON), KC_Q,     KC_J,    KC_K,    KC_X,     KC_B,  KC_M,    KC_W,    KC_V,    KC_Z,  KC_ENTER,
+          _______,        KC_LCTRL,          KC_LALT,  KC_LGUI, KC_LSFT, KC_SPACE, KC_NO, KC_RCTL, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT
           ),
 
   [_NUMBER] = LAYOUT_planck_grid(
--- keyboards/planck/keymaps/tylerwince/rules.mk
+++ keyboards/planck/keymaps/tylerwince/rules.mk
@@ -1,5 +1,6 @@
 # Set any rules.mk overrides for your specific keymap here.
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
+AUDIO_ENABLE=no
 LINK_TIME_OPTIMIZATION_ENABLE=yes
 COMMAND_ENABLE=no
 MOUSEKEY_ENABLE=yes
