bbf653f423e99103d3b8bac2d1f8ca540e42e1dc
Tyler Wince <tyler@myndshft.com>
2019-07-24 16:41:57 -0700

    updated config

--- keyboards/planck/keymaps/tylerwince/config.h
+++ keyboards/planck/keymaps/tylerwince/config.h
@@ -4,11 +4,8 @@
   Set any config.h overrides for your specific keymap here.
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
-#define TAPPING_FORCE_HOLD
-#define IGNORE_MOD_TAP_INTERRUPT
 #define MOUSEKEY_INTERVAL 16
 #define MOUSEKEY_DELAY 0
 #define MOUSEKEY_TIME_TO_MAX 60
 #define MOUSEKEY_MAX_SPEED 7
 #define MOUSEKEY_WHEEL_DELAY 0
-/* #define EECONFIG_RGB_MATRIX (uint32_t *)16 */
