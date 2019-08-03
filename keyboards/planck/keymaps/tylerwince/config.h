8036827bae87c3850bbe542ea8b8d6db6f8be38b
Tyler Wince <tyler@myndshft.com>
2019-08-03 10:55:33 -0700

    updated colors

--- keyboards/planck/keymaps/tylerwince/config.h
+++ keyboards/planck/keymaps/tylerwince/config.h
@@ -4,7 +4,6 @@
   Set any config.h overrides for your specific keymap here.
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
-#define MOUSEKEY_INTERVAL 16
 #define MOUSEKEY_DELAY 0
 #define MOUSEKEY_TIME_TO_MAX 60
 #define MOUSEKEY_MAX_SPEED 7
